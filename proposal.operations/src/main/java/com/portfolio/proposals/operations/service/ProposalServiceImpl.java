3abce0019e6f4cf20b8f0f8ac9109258ffba3feb
Igor de campos <146300081+igorcampos-dev@users.noreply.github.com>
2024-07-24 09:51:48 -0300

    Update ProposalServiceImpl.java

--- proposal.operations/src/main/java/com/portfolio/proposals/operations/service/ProposalServiceImpl.java
+++ proposal.operations/src/main/java/com/portfolio/proposals/operations/service/ProposalServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@SuppressWarnings("unused")
 public class ProposalServiceImpl implements ProposalService {
 
     private final ProposalRepository proposalRepository;
